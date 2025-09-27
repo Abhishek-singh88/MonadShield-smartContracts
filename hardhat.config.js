9eae5008a858b291daaccaf0dd3d1d5860261770
Abhishek Singh <abhisheksingh4928@gmail.com>
2025-09-27 17:50:19 +0530

    adding monad rpc url

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,38 @@
-require("@nomicfoundation/hardhat-toolbox");
+const { ethers } = require("ethers");
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+require("dotenv").config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "0x" + "0".repeat(64);
+const MONAD_RPC_URL = process.env.MONAD_RPC_URL || "https://testnet-rpc.monad.xyz";
+
 module.exports = {
-  solidity: "0.8.28",
+  solidity: {
+    version: "0.8.19",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545",
+      chainId: 1337,
+    },
+    monad: {
+      url: MONAD_RPC_URL,
+      accounts: PRIVATE_KEY !== "0x" + "0".repeat(64) ? [PRIVATE_KEY] : [],
+      chainId: 10143,
+      
+      maxFeePerGas:  ethers.utils.parseUnits("60", "gwei").toString(), 
+      maxPriorityFeePerGas: ethers.utils.parseUnits("2", "gwei").toString(),
+
+      timeout: 60000,
+    },
+  },
 };
